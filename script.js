688b6bd72968c6420061c9b4680dd64c836ee074
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-14 12:31:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   let price = document.querySelectorAll(".price");
+    
+    let total = 0;
+	    for (let i = 0; i < price.length; i++) {
+        total += parseFloat(price[i].innerText) ; 
+    }
+let existingTotalRow = document.getElementById("ans");
+    if (existingTotalRow) {
+        existingTotalRow.remove();
+    }
+	 let table = document.querySelector("table");
+
+    let newRow = document.createElement("tr");
+    newRow.id = "ans"; 
+    let totalCell = document.createElement("td");
+    totalCell.colSpan = 2; 
+    totalCell.innerText = "Total Price: ₹" + total.toFixed(2);
+    newRow.appendChild(totalCell);
+    table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
